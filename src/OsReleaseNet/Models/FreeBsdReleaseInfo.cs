0c04c12c4afc7ca6c96f4c8bcaeca3b07af420a1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-10 11:03:52 +0100

    add xml doc comments

--- src/OsReleaseNet/Models/FreeBsdReleaseInfo.cs
+++ src/OsReleaseNet/Models/FreeBsdReleaseInfo.cs
@@ -1,5 +1,18 @@
+/*
+    OsReleaseNet
+    Copyright (c) 2020-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 namespace AlastairLundy.OsReleaseNet;
 
+/// <summary>
+/// Represents a FreeBSD Distribution's OsRelease file and information contained therein.
+/// </summary>
+/// <remarks>All trademarks mentioned belong to their respective owners.</remarks>
 public class FreeBsdReleaseInfo
 {
     internal FreeBsdReleaseInfo()
@@ -16,6 +29,18 @@ public class FreeBsdReleaseInfo
         BugReportUrl = string.Empty;
     }
     
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="version"></param>
+    /// <param name="identifier"></param>
+    /// <param name="prettyName"></param>
+    /// <param name="versionId"></param>
+    /// <param name="homeUrl"></param>
+    /// <param name="bugReportUrl"></param>
+    /// <param name="ansiColor"></param>
+    /// <param name="cpeName"></param>
     public FreeBsdReleaseInfo(string name, string version, string identifier, string prettyName, string versionId, string homeUrl, string bugReportUrl, string ansiColor, string cpeName)
     {
         Name = name;
@@ -33,16 +58,39 @@ public class FreeBsdReleaseInfo
 
     public string CpeName { get; set; }
     
+    /// <summary>
+    /// The name of the FreeBSD Distribution.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// The FreeBSD distribution display version. 
+    /// </summary>
+    /// <remarks>This string should not be parsed into a <see cref="Version"/> object as it may contain a version number and version name. Use <see cref="VersionId"/> instead.</remarks>
     public string Version { get; set; }
-    
+
+    /// <summary>
+    /// The FreeBSD distribution's identifier.
+    /// </summary>
     public string Identifier { get; set; }
 
+    /// <summary>
+    /// The pretty name/display name for the FreeBSD distribution.
+    /// </summary>
     public string PrettyName { get; set; }
 
+    /// <summary>
+    /// The FreeBSD distribution's version number.
+    /// </summary>
     public string VersionId { get; set; }
 
+    /// <summary>
+    /// The FreeBSD distribution's homepage/website.
+    /// </summary>
     public string HomeUrl { get; set; }
+    
+    /// <summary>
+    /// The FreeBSD distribution's bug reporting website url (if provided).
+    /// </summary>
     public string BugReportUrl { get; set; }
 }
