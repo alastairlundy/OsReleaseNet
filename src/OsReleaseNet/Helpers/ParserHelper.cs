b2d5b33b42697c61d0368b44c9a1b58eb1cebae1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-11 17:50:58 +0000

    Update ParserHelper.cs

--- src/OsReleaseNet/Helpers/ParserHelper.cs
+++ src/OsReleaseNet/Helpers/ParserHelper.cs
@@ -25,11 +25,11 @@ internal static class ParserHelper
     /// <summary>
     /// Removes unwanted characters from an array of strings.
     /// </summary>
-    /// <param name="resultArray">The input array containing strings that may contain unwanted characters.</param>
+    /// <param name="results">The input array containing strings that may contain unwanted characters.</param>
     /// <returns>An array of strings with unwanted characters removed.</returns>
-    internal static IEnumerable<string> RemoveUnwantedCharacters(IEnumerable<string> resultArray)
+    internal static IEnumerable<string> RemoveUnwantedCharacters(IEnumerable<string> results)
     {
-        IEnumerable<string> newResults = resultArray
+        IEnumerable<string> newResults = results
             .Where(x => string.IsNullOrWhiteSpace(x) == false && x.Equals(string.Empty) == false)
             .Select(x => x.Replace('"'.ToString(), string.Empty));
         
